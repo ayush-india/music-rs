cc4e6da4c9858f96ce7b117db1479097ea7e6daa
ayushmishra <mail81mitthu@gmail.com>
2023-03-18 12:25:00 +0530

    removed cli and added args for path

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,13 @@
 use music_player::*;
-use std::io::*;
+use std::env::*;
 use std::path::*;
 fn main() {
     // play_audio(&PathBuf::from("yeet/2055.mp3"));
 
-    println!("Enter song path: ");
-    let mut inp = String::new();
-
-    stdin().read_line(&mut inp).expect("Unable to parse input");
-
-    let help = iter_dirs(&PathBuf::from(inp.trim())); 
+    let args: Vec<String> = args().collect();
+    let help = iter_dirs(&PathBuf::from(args[1].trim()));
     for i in help {
         println!("{}", i.display());
+        play_audio(&i);
     }
-
 }
