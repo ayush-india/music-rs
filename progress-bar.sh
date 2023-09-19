559ee74fb4acd7fffb811141639bfc8a1ae1d376
ayushmishra <mail81mitthu@gmail.com>
2023-09-19 14:28:11 +0530

    got the progress bar working will imporve it

--- progress-bar.sh
+++ progress-bar.sh
@@ -1,11 +1,6 @@
-#!/usr/bin/env bash
-duration=100
-percentage() { printf "%s%%" $(( ((elapsed)*100)/(duration)*100/100 )); }
-clean_line() { printf "\r"; }
-
+duration=5
 for (( elapsed=1; elapsed<=duration; elapsed=elapsed+1 )); do
-    percentage
-    sleep 1
-    clean_line
+    printf "%s%%" $(( ((elapsed)*100)/(duration)*100/100 ))
+    sleep 0.2
+    printf "\r"
 done
-clean_line
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
-use cmd_lib::*;
+use std::process::Command;
 fn main() {
-    run_cmd!(mkdir pit);
 
+
+    let arg = format!(r#"
+duration=5
+for (( elapsed=1; elapsed<=duration; elapsed=elapsed+1 )); do
+    printf "%s%%" $(( ((elapsed)*100)/(duration)*100/100 ))
+    sleep 1.2 
+    printf "\r"
+done
+    "#); // TODO: remove the sleep from the bash cmd and add it to rust coz it is interferring with the loop
+
+    Command::new("bash").arg("-c").arg(arg).spawn();
 }
