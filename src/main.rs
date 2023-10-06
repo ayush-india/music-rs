82bff36a26a22e4b5b299c27e7849838789fce26
ayushmishra <mail81mitthu@gmail.com>
2023-10-06 22:24:09 +0530

    got the progress bar working

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,24 @@
 use std::process::Command;
+use std::{thread, time};
 fn main() {
 
+    let length_song: i16 = 6;
+    progress(length_song);
+}
 
-    let arg = format!(r#"
-duration=5
-for (( elapsed=1; elapsed<=duration; elapsed=elapsed+1 )); do
-    printf "%s%%" $(( ((elapsed)*100)/(duration)*100/100 ))
-    sleep 1.2 
-    printf "\r"
-done
-    "#); // TODO: remove the sleep from the bash cmd and add it to rust coz it is interferring with the loop
-
-    Command::new("bash").arg("-c").arg(arg).spawn();
+fn progress(duration: i16) {
+    let mut elapsed = 1; // see the meaning of elapsed ;)
+    let mut percentage = 0;
+    while elapsed <= duration {
+      // TODO: figure out how to get duration working  
+    // for (( elapsed=1; elapsed<=duration; elapsed=elapsed+1 )); do
+    //     printf "%s%%" $(( ((elapsed)*100)/(duration)*100/100 ))
+        percentage = ((elapsed)*100)/(duration)*100/100;
+        
+        let s = format!("{percentage}%"); // could not get it working in raw string (string slice)
+        Command::new("printf").arg(s).spawn();
+        Command::new("printf").arg(r#"\r"#).spawn();
+        thread::sleep(time::Duration::new(1,0));
+        elapsed += 1;
+    }
 }
