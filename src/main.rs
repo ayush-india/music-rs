ac6ff7f585d308ecb90e02fd1ed5ff3ec23516df
ayushmishra <mail81mitthu@gmail.com>
2023-10-08 19:41:21 +0530

    got % working

--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,15 @@ use std::process::Command;
 use std::{thread, time};
 fn main() {
 
-    let length_song: i16 = 6;
+    let length_song: i16 = 10;
     progress(length_song);
 }
 
 fn progress(duration: i16) {
     let mut elapsed = 1; // see the meaning of elapsed ;)
-    let mut percentage = 0;
     while elapsed <= duration {
-      // TODO: figure out how to get duration working  
-    // for (( elapsed=1; elapsed<=duration; elapsed=elapsed+1 )); do
-    //     printf "%s%%" $(( ((elapsed)*100)/(duration)*100/100 ))
-        percentage = ((elapsed)*100)/(duration)*100/100;
-        
-        let s = format!("{percentage}%"); // could not get it working in raw string (string slice)
+        let percentage = ((elapsed)*100)/(duration)*100/100;
+        let s = format!("{percentage}%% "); // the dobule %% is because % is some kind of specifer in prinft see man printf 
         Command::new("printf").arg(s).spawn();
         Command::new("printf").arg(r#"\r"#).spawn();
         thread::sleep(time::Duration::new(1,0));
