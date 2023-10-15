8da2f1e3899a9022661ad4dd82d888d513fc015a
ayushmishra <mail81mitthu@gmail.com>
2023-10-15 22:52:54 +0530

    old stuff

--- src/main.rs
+++ src/main.rs
@@ -8,29 +8,8 @@ use std::process::Command;
 use std::{thread, time};
 
 fn main() {
-    let file_path = Path::new("deez.mp3");
-    let selected_file = Probe::open(file_path)
-        .expect("Error: bad path")
-        .read()
-        .expect("ERROR: unable to read");
-    let duration = selected_file.properties().duration();
-    play(file_path.to_str().unwrap(), duration)
 }
 
-fn play(file_path: &str, duration: time::Duration) {
-    // Get a output stream handle to the default physical sound device
-    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    // Load a sound from a file, using a path relative to Cargo.toml
-    let file = BufReader::new(File::open(file_path).unwrap());
-    // Decode that sound file into a source
-    let source = Decoder::new(file).unwrap();
-    // Play the sound directly on the device
-    stream_handle.play_raw(source.convert_samples());
-
-    // The sound plays in a separate audio thread,
-    // so we need to keep the main thread alive while it's playing.
-    std::thread::sleep(duration);
-}
 fn progress(duration: i16) {
     let mut elapsed = 1; // see the meaning of elapsed ;)
     while elapsed <= duration {
